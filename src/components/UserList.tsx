07732672097a4367b0c4bca6fac7222133f7d179
Anton Eliseev <anton_saintp@mail.ru>
2023-11-07 14:58:52 +0300

    added useTypedSelector hook, fetchUsers action-creator

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store/action-creators/user";
 
-export default function UserList() {
-  const state = useSelector((state) => state);
-  console.log(state)
-  return <div>UserList</div>;
-}
+const UserList: React.FC = () => {
+  const { users, error, loading } = useTypedSelector((state) => state.user);
+  const dispatch : any = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
+  if (loading) {
+    return <h1>Идёт загрузка...</h1>;
+  }
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
+
+  return (
+    <div>
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default UserList;
